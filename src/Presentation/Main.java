a7fa7073912424a45519e39120f59173ca8d0d06
saradiberardino9712 <sara.9712@hotmail.it>
2018-05-25 14:13:27 +0200

    modifica implementazione

--- src/Presentation/Main.java
+++ src/Presentation/Main.java
@@ -1,4 +1,4 @@
-package View.LoginPage;
+package Presentation;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("RegistrazionePage.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
