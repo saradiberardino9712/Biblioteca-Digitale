6a62ea94a7739dac7b942efcc0a912c32e21ee2e
saradiberardino9712 <sara.9712@hotmail.it>
2018-06-06 17:24:09 +0200

    modifiche package view

--- src/src/View/FrontController/LoginPageController.java
+++ src/src/View/FrontController/LoginPageController.java
@@ -1,5 +1,51 @@
 package View.FrontController;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginPageController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private Button btnAccedi;
+
+    @FXML
+    private Button btnRegistrati;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    void initialize() {
+        assert txtEmail != null : "fx:id=\"txtEmail\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert lblEmail != null : "fx:id=\"lblEmail\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert lblPassword != null : "fx:id=\"lblPassword\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert btnAccedi != null : "fx:id=\"btnAccedi\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert btnRegistrati != null : "fx:id=\"btnRegistrati\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert txtPassword != null : "fx:id=\"txtPassword\" was not injected: check your FXML file 'LoginPage.fxml'.";
+        assert lblStatus != null : "fx:id=\"lblStatus\" was not injected: check your FXML file 'LoginPage.fxml'.";
+
+    }
 }
