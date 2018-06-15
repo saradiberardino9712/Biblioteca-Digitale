9f2a631c1474629549699653d9f0a2d826921625
Davisim <38098547+Davisim@users.noreply.github.com>
2018-06-15 11:52:38 +0200

    caricamento interfacce e controller

--- src/src/View/FrontController/RegistrazionePagecontroller.java
+++ src/src/View/FrontController/RegistrazionePagecontroller.java
@@ -1,5 +1,28 @@
 package View.FrontController;
 
-public class RegistrazionePagecontroller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+
+public class RegistrazionePagecontroller implements Initializable{
+	
+	@FXML
+	public ComboBox<String> combobox;
+	
+	ObservableList<String> list = FXCollections.observableArrayList("Utente base", "Utente Privilegiato", "Acquisitore", "Supervisore", "Trascrittore", "Revisore", "Manager");
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		combobox.setItems(list);
+		
+	}
 
 }
