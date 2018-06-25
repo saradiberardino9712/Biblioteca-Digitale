76bda6c13be1cb3d6169022a81c7cc4ca5ab4cbb
saradiberardino9712 <sara.9712@hotmail.it>
2018-06-25 14:57:42 +0200

    caricamento script

---
+++ inserimento tabella ruolo.sql
@@ -0,0 +1,9 @@
+use biblioteca_digitale;
+
+insert into ruolo(nome_ruolo) values ('Utente base');
+insert into ruolo(nome_ruolo) values ('Utente privilegiato');
+insert into ruolo(nome_ruolo) values ('Acquisitore');
+insert into ruolo(nome_ruolo) values ('Supervisore');
+insert into ruolo(nome_ruolo) values ('Trascrittore');
+insert into ruolo(nome_ruolo) values ('Revisore');
+insert into ruolo(nome_ruolo) values ('Manager');
