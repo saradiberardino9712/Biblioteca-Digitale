8053d0ec92d57803e39042bc0f3cc6217741f244
saradiberardino9712 <sara.9712@hotmail.it>
2018-06-27 19:12:22 +0200

    caricamento classe ruolo

--- src/src/Business/Model/Ruolo.java
+++ src/src/Business/Model/Ruolo.java
@@ -1,5 +1,17 @@
 package Business.Model;
 
 public class Ruolo {
-
+	private String nome_ruolo;
+	
+	public Ruolo(String nome_ruolo) {
+		this.nome_ruolo=nome_ruolo;
+	}
+	
+	public void setNomeruolo(String newnomeruolo) {
+		this.nome_ruolo=newnomeruolo;
+	}
+	
+	public String getNome () {
+		return nome_ruolo;
+	}
 }
