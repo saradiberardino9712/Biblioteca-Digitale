b46535bce3eb81fcaf376e530ea7c70b891412e7
Davisim <38098547+Davisim@users.noreply.github.com>
2018-06-29 12:42:53 +0200

    Interfacce acquisitore revisore e trascrittore

---
+++ src/src/View/FrontController/AcquisitorePageController.java
@@ -0,0 +1,44 @@
+package View.FrontController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class AcquisitorePageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCarica;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    void initialize() {
+        assert btnCarica != null : "fx:id=\"btnCarica\" was not injected: check your FXML file 'AcquisitorePage.fxml'.";
+        assert btnLogout != null : "fx:id=\"btnLogout\" was not injected: check your FXML file 'AcquisitorePage.fxml'.";
+
+    }
+    
+    public void Logout(ActionEvent event) throws Exception {
+    	((Node)event.getSource()).getScene().getWindow().hide();
+    	Stage primaryStage = new Stage();
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/LoginPage.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+}
