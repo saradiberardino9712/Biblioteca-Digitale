aa735af2523f0ce586e872f8cde51f7a52f960a4
Davisim <38098547+Davisim@users.noreply.github.com>
2018-07-02 08:56:03 +0200

    interfaccia utente privilegiato, modifica interfaccia utente base

---
+++ src/src/View/FrontController/UtentePrivilegiatoPageController.java
@@ -0,0 +1,44 @@
+package View.FrontController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class UtentePrivilegiatoPageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnRicerca;
+
+    @FXML
+    void initialize() {
+        assert btnLogout != null : "fx:id=\"btnLogout\" was not injected: check your FXML file 'UtentePrivilegiatoPage.fxml'.";
+        assert btnRicerca != null : "fx:id=\"btnRicerca\" was not injected: check your FXML file 'UtentePrivilegiatoPage.fxml'.";
+
+    }
+    
+    public void Logout(ActionEvent event) throws Exception {
+    	((Node)event.getSource()).getScene().getWindow().hide();
+    	Stage primaryStage = new Stage();
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/LoginPage.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+}
