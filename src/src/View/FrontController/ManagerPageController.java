dcbca265c241740ee27c5383fc0036d42c3b4b52
Davisim <38098547+Davisim@users.noreply.github.com>
2018-07-03 17:55:50 +0200

    Collegamento interfacce utenti con interfaccia ricerca

--- src/src/View/FrontController/ManagerPageController.java
+++ src/src/View/FrontController/ManagerPageController.java
@@ -2,8 +2,15 @@ package View.FrontController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class ManagerPageController {
 
@@ -52,6 +59,24 @@ public class ManagerPageController {
         assert btnSupervisionaImmagini != null : "fx:id=\"btnSupervisionaImmagini\" was not injected: check your FXML file 'ManagerPage.fxml'.";
         assert btnLogout != null : "fx:id=\"btnLogout\" was not injected: check your FXML file 'ManagerPage.fxml'.";
 
+    }
+    
+    public void Logout(ActionEvent event) throws Exception {
+    	((Node)event.getSource()).getScene().getWindow().hide();
+    	Stage primaryStage = new Stage();
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/LoginPage.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+    
+    public void Ricerca(ActionEvent event) throws Exception {
+    	((Node)event.getSource()).getScene().getWindow().hide();
+    	Stage primaryStage = new Stage();
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/RicercaOpere.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
     }
 }
 
--- src/src/View/FrontController/UtenteBaseController.java
+++ src/src/View/FrontController/UtenteBaseController.java
@@ -42,4 +42,13 @@ public class UtenteBaseController {
 		primaryStage.setScene(scene);
 		primaryStage.show();
     }
+    
+    public void Ricerca(ActionEvent event) throws Exception {
+    	((Node)event.getSource()).getScene().getWindow().hide();
+    	Stage primaryStage = new Stage();
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/RicercaOpere.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
 }
