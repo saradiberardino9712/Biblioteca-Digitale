4e9e41d1222f2a62e71ad6dcf70245ef54c35a3a
Davisim <38098547+Davisim@users.noreply.github.com>
2018-07-03 17:13:33 +0200

    interfaccia manager e amministratore

--- src/src/View/FrontController/ManagerPageController.java
+++ src/src/View/FrontController/ManagerPageController.java
@@ -1,5 +1,57 @@
 package View.FrontController;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ManagerPageController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnRicerca;
+
+    @FXML
+    private Button btnAssegnaOpera;
+
+    @FXML
+    private Button btnRevisiona;
+
+    @FXML
+    private Button btnTrascrivi;
+
+    @FXML
+    private Button btnAssegnaPagine;
+
+    @FXML
+    private Button btnConsentiPubblicazione;
+
+    @FXML
+    private Button btnGestisciLivello;
+
+    @FXML
+    private Button btnSupervisionaImmagini;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    void initialize() {
+        assert btnRicerca != null : "fx:id=\"btnRicerca\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnAssegnaOpera != null : "fx:id=\"btnAssegnaOpera\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnRevisiona != null : "fx:id=\"btnRevisiona\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnTrascrivi != null : "fx:id=\"btnTrascrivi\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnAssegnaPagine != null : "fx:id=\"btnAssegnaPagine\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnConsentiPubblicazione != null : "fx:id=\"btnConsentiPubblicazione\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnGestisciLivello != null : "fx:id=\"btnGestisciLivello\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnSupervisionaImmagini != null : "fx:id=\"btnSupervisionaImmagini\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+        assert btnLogout != null : "fx:id=\"btnLogout\" was not injected: check your FXML file 'ManagerPage.fxml'.";
+
+    }
 }
+
