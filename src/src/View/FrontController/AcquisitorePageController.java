7b0c80e26347a40dc5dc42c84275b6cfdea62664
Davisim <38098547+Davisim@users.noreply.github.com>
2018-07-31 13:22:25 +0200

    correzione carica immagini

--- src/src/View/FrontController/AcquisitorePageController.java
+++ src/src/View/FrontController/AcquisitorePageController.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
--- src/src/View/FrontController/CaricaImmaginiController.java
+++ src/src/View/FrontController/CaricaImmaginiController.java
@@ -1,16 +1,9 @@
 package View.FrontController;
 
-import java.io.File;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
 
 public class CaricaImmaginiController {
 
@@ -21,9 +14,6 @@ public class CaricaImmaginiController {
     private URL location;
 
     @FXML
-    private ListView listCarica;
-
-    @FXML
     private Button btnSeleziona1;
 
     @FXML
@@ -31,37 +21,10 @@ public class CaricaImmaginiController {
 
     @FXML
     void initialize() {
-        assert listCarica != null : "fx:id=\"txtCarica\" was not injected: check your FXML file 'CaricaImmagini.fxml'.";
         assert btnSeleziona1 != null : "fx:id=\"btnSeleziona1\" was not injected: check your FXML file 'CaricaImmagini.fxml'.";
         assert btnSeleziona2 != null : "fx:id=\"btnSeleziona2\" was not injected: check your FXML file 'CaricaImmagini.fxml'.";
 
     }
-    
-    public void Seleziona1(ActionEvent event) {
-    	FileChooser fc = new FileChooser();
-    	fc.getExtensionFilters().addAll(new ExtensionFilter("FILE JPG", "*.jpg"));
-    	File selectedFile = fc.showOpenDialog(null);
-    	
-    	if(selectedFile != null) {
-    		listCarica.getItems().add(selectedFile.getAbsolutePath());
-    	}
-    	else {
-    		System.out.println("file is not valid");
-    	}
-    }
-    
-    public void Seleziona2(ActionEvent event) {
-    	FileChooser fc = new FileChooser();
-    	fc.getExtensionFilters().addAll(new ExtensionFilter("FILE JPG", "*.jpg"));
-    	List<File> selectedFiles = fc.showOpenMultipleDialog(null);
-    	
-    	if(selectedFiles != null) {
-    		for(int i = 0; i < selectedFiles.size(); i++) {
-    		listCarica.getItems().add(selectedFiles.get(i).getAbsolutePath());
-    		}
-    	}
-    	else {
-    		System.out.println("file is not valid");
-    	}
-    }
 }
+
+
