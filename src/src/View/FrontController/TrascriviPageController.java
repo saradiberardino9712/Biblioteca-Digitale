2395d2114aa469b32c4062829449aebcd74b8684
Davisim <38098547+Davisim@users.noreply.github.com>
2018-08-20 11:47:47 +0200

    aggiornamento

--- src/src/View/FrontController/TrascriviPageController.java
+++ src/src/View/FrontController/TrascriviPageController.java
@@ -2,7 +2,12 @@ package View.FrontController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.web.HTMLEditor;
 
 public class TrascriviPageController {
 
@@ -13,8 +18,26 @@ public class TrascriviPageController {
     private URL location;
 
     @FXML
-    void initialize() {
+    private ImageView immagine;
 
+    @FXML
+    private Button btnImmagine;
+    
+    @FXML
+    private HTMLEditor TextEditor;
+
+
+    @FXML
+    void initialize() {
+        assert immagine != null : "fx:id=\"immagine\" was not injected: check your FXML file 'TrascriviPage.fxml'.";
+        assert btnImmagine != null : "fx:id=\"btnImmagine\" was not injected: check your FXML file 'TrascriviPage.fxml'.";
+        assert TextEditor != null : "fx:id=\"TextEditor\" was not injected: check your FXML file 'TrascriviPage.fxml'.";
+
+    }
+    
+    public void Immagine(ActionEvent event) {
+    	Image image = new Image ("/View/img/manoscritto.jpg");
+    	immagine.setImage(image);
     }
     
 }
