b22478eeba638f27ade3d0a979d1495261295191
Davisim <38098547+Davisim@users.noreply.github.com>
2018-08-25 15:32:56 +0200

    paginatore, aggiornamento

--- src/src/View/FrontController/CaricaImmaginePaginatorePageController.java
+++ src/src/View/FrontController/CaricaImmaginePaginatorePageController.java
@@ -2,13 +2,11 @@ package View.FrontController;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
-
 import javax.imageio.ImageIO;
-
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -19,13 +17,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Pagination;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.util.Callback;
-import javafx.scene.layout.AnchorPane;
 
 public class CaricaImmaginePaginatorePageController {
 
@@ -46,6 +42,8 @@ public class CaricaImmaginePaginatorePageController {
     @FXML
     private Hyperlink linkIndietro;
 
+    
+
     @FXML
     void initialize() {
         assert PaginatoreOpere != null : "fx:id=\"PaginatoreOpere\" was not injected: check your FXML file 'CaricaImmaginePaginatorePage.fxml'.";
@@ -54,124 +52,33 @@ public class CaricaImmaginePaginatorePageController {
 
     }
     
-    private void SelezionaMomentaneo(ActionEvent event) {
-    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    	/*
-    	Stage stage = (Stage) anchor.getScene().getWindow();
-    	
-    	OpenDirectoryChooser(stage);
-    	PaginatoreOpere.setPageFactory(new Callback<Integer, Node>(){
-    		@Override
-    		public Node call(Integer pageIndex) {
-    			return createPage(pageIndex);
-    		}
-    	});*/
-    	//Stage stage = (Stage) anchor.getScene().getWindow();
-        
-        OpenDirectoryChooser(stage);        
-        
-        PaginatoreOpere.setPageFactory(new Callback<Integer, Node>() {
-           @Override
-           public Node call(Integer pageIndex) {
-               return createPage(pageIndex);
-           }
-       });
-    }
-    
-    /*
-    public VBox createPage(int index) {
-    	ImageView imageView = new ImageView();
-    	
-    	File file = new filesJpg[index];
-    	try {
-    		BufferedImage bufferedImage = ImageIO.read(file);
-    		Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-    		imageView.setImage(image);
-    		imageView.setFitWidth(400);
-    		imageView.setFitHeight(360);
-    		//imageView.setPreserveRatio(true);
-    		
-    		imageView.setSmooth(true);
-    		imageView.setCache(true);
-    	} catch (IOException ex) {
-    		
-    	}
-    	
-    	VBox pageBox = new VBox();
-    	pageBox.getChildren().add(imageView);
-    	
-    	return pageBox;
-    }
-    
-    private void OpenDirectoryChooser(Stage parent) {
-    	DirectoryChooser directoryChooser = new DirectoryChooser();
-    	File selectedDirectory = directoryChooser.showDialog(parent);
-    	
-    	if(selectedDirectory != null) {
-    		FilenameFilter filterJpg = new FilenameFilter () {
-    			@Override
-    			public boolean accept(File dir, String name) {
-    				return name.toLowerCase().endsWith(".jpg");
-    			}
-    		};
-    		
-    		filesJpg = selectedDirectory.listFiles(filterJpg);
-    	}
-    }
-}
-*/
-    public VBox createPage(int index) {
-    	 
-        ImageView imageView = new ImageView();
- 
-        File file = filesJpg[index];
-        try {
-            BufferedImage bufferedImage = ImageIO.read(file);
-            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-            imageView.setImage(image);
-            imageView.setFitWidth(400);
-            imageView.setFitHeight(360);
-           // imageView.setPreserveRatio(true);
-            
-            imageView.setSmooth(true);
-            imageView.setCache(true);
-        } catch (IOException ex) {
-            
-        }
-         
-        VBox pageBox = new VBox();
-        pageBox.getChildren().add(imageView);
-        
-        
-        return pageBox;
-    }  
-    
-    
-    
-    
-    public void initialize(URL url, ResourceBundle rb) {       
-        
-        
-    }    
-    
-    private void OpenDirectoryChooser(Stage parent) {
-        
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        File selectedDirectory = directoryChooser.showDialog(parent);
- 
-        if (selectedDirectory != null) {
-            FilenameFilter filterJpg = new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return name.toLowerCase().endsWith(".jpg");
-                }
-            };
- 
-            filesJpg = selectedDirectory.listFiles(filterJpg);
-            
-        }
-    }
-    
-    
-}
 
+    private FileChooser fc;
+    //private File fp;
+    
+    public void SelezionaMomentaneo(ActionEvent event) {
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	fc = new FileChooser();
+    	fc.getExtensionFilters().addAll(new ExtensionFilter("FILE JPG", "*.jpg"));
+    	
+    	List <File>fp = fc.showOpenMultipleDialog(stage);
+    	
+    	try {
+    		BufferedImage bufferedImage = ImageIO.read((File) fp);
+    		Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+    		PaginatoreOpere.setPageFactory((Callback<Integer, Node>) image);
+    	} catch (IOException e) {
+    		System.err.println(e.getMessage());
+    	}
+    }
+    
+    
+    public void indietro(ActionEvent event) throws Exception {
+    	((Node)event.getSource()).getScene().getWindow().hide();
+    	Stage primaryStage = new Stage();
+    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/CaricaImmagini.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+}
