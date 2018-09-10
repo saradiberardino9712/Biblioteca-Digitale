563bc9ae856bb8e1166f0f390a853c53ea1a3790
Davisim <38098547+Davisim@users.noreply.github.com>
2018-09-10 13:06:23 +0200

    modifiche paginatore

--- src/src/View/FrontController/CaricaImmaginePaginatorePageController.java
+++ src/src/View/FrontController/CaricaImmaginePaginatorePageController.java
@@ -2,24 +2,25 @@ package View.FrontController;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
+
 import javax.imageio.ImageIO;
+
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Pagination;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
@@ -33,8 +34,6 @@ public class CaricaImmaginePaginatorePageController {
 
     @FXML
     private Pagination PaginatoreOpere;
-    
-    File filesJpg[];
 
     @FXML
     private Button btnSelezionaMomentaneo;
@@ -42,7 +41,18 @@ public class CaricaImmaginePaginatorePageController {
     @FXML
     private Hyperlink linkIndietro;
 
+    @FXML
+    void indietro(ActionEvent event) {
+
+    }
     
+    File filesJpg[];
+    
+    @FXML
+    private AnchorPane anchor;
+    
+    @FXML
+    private Pagination pagination;
 
     @FXML
     void initialize() {
@@ -52,33 +62,56 @@ public class CaricaImmaginePaginatorePageController {
 
     }
     
-
-    private FileChooser fc;
-    //private File fp;
-    
     public void SelezionaMomentaneo(ActionEvent event) {
     	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    	fc = new FileChooser();
-    	fc.getExtensionFilters().addAll(new ExtensionFilter("FILE JPG", "*.jpg"));
     	
-    	List <File>fp = fc.showOpenMultipleDialog(stage);
-    	
-    	try {
-    		BufferedImage bufferedImage = ImageIO.read((File) fp);
-    		Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-    		PaginatoreOpere.setPageFactory((Callback<Integer, Node>) image);
-    	} catch (IOException e) {
-    		System.err.println(e.getMessage());
-    	}
+    	openDirectoryChooser(stage);
+    	pagination.setPageFactory(new Callback<Integer, Node>(){
+    		@Override
+    		public Node call (Integer pageIndex) {
+    			return createPage(pageIndex);
+    		}
+    	});
+
     }
     
+    public VBox createPage(int index) {
+    	ImageView imageView = new ImageView();
+    	
+    	File file = filesJpg[index];
+    	try {
+    		BufferedImage bufferedImage = ImageIO.read(file);
+    		Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+    		imageView.setImage(image);
+    		imageView.setFitWidth(400);
+    		imageView.setFitHeight(360);
+    		
+    		imageView.setSmooth(true);
+    		imageView.setCache(true);
+    	} catch (IOException ex) {
+    		
+    	}
+    	
+    	VBox pageBox = new VBox();
+    	pageBox.getChildren().add(imageView);
+    	
+    	return pageBox;
+    }
     
-    public void indietro(ActionEvent event) throws Exception {
-    	((Node)event.getSource()).getScene().getWindow().hide();
-    	Stage primaryStage = new Stage();
-    	BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/View/javaFX/CaricaImmagini.fxml"));
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+    private void openDirectoryChooser(Stage parent) {
+    	DirectoryChooser directoryChooser = new DirectoryChooser();
+    	File selectedDirectory = directoryChooser.showDialog(parent);
+    	
+    	if(selectedDirectory != null) {
+    		FilenameFilter filterJpg = new FilenameFilter() {
+    			
+    			public boolean accpet(File dir, String name) {
+    				return name.toLowerCase().endsWith(".jpg");
+    			}
+    		};
+    		
+    	}
+    	
+    	filesJpg = selectedDirectory.listFiles();
     }
 }
