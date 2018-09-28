2c07b423d1de282e5f5f2c7af3dbf05ab0cdc722
Davisim <38098547+Davisim@users.noreply.github.com>
2018-09-28 12:20:47 +0200

    correzione TrascriviPageController

--- src/src/View/FrontController/TrascriviPageController.java
+++ src/src/View/FrontController/TrascriviPageController.java
@@ -1,13 +1,24 @@
 package View.FrontController;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class TrascriviPageController {
 
@@ -35,9 +46,23 @@ public class TrascriviPageController {
 
     }
     
+    private FileChooser fc;
+    private File fp;
+    
     public void Immagine(ActionEvent event) {
-    	Image image = new Image ("/View/img/manoscritto.jpg");
-    	immagine.setImage(image);
+    	Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	fc = new FileChooser();
+    	fc.getExtensionFilters().addAll(new ExtensionFilter("FILE JPG", "*.jpg"));
+    	
+    	this.fp = fc.showOpenDialog(stage);
+    	
+    	try {
+    		BufferedImage bufferedImage = ImageIO.read(fp);
+    		Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+    		immagine.setImage(image);
+    	} catch (IOException e) {
+    		System.err.println(e.getMessage());
+    	}
     }
     
 }
