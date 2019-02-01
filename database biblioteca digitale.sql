ed019bdf8777ae07bb900b5ace1f31dc1c0a32f9
saradiberardino9712 <sara.9712@hotmail.it>
2019-02-01 11:07:08 +0100

    Modifica db

--- database biblioteca digitale.sql
+++ database biblioteca digitale.sql
@@ -50,7 +50,7 @@ constraint consulta_opera foreign key (ID_opera) references opera(ID)
 create table immagine (
 ID integer unsigned primary key not null auto_increment,
 numero_pagina smallint not null,
-stato enum('in caricamento','in acquisizione','in attesa supervisione','in revisione acquisizione','acquisito','eliminata') not null,
+stato enum('in caricamento','in acquisizione','in attesa supervisione','in caricamento acquisizione','in revisione acquisizione','acquisito','eliminata') not null,
 url varchar(150) not null,
 ID_utente integer unsigned not null,
 ID_opera integer unsigned not null,
@@ -80,6 +80,7 @@ orario timestamp not null,
 descrizione varchar(250),
 vista boolean default false,
 IDutentenot integer unsigned not null,
+IDruolonot integer unsigned not null,
 ID_utente integer unsigned not null,
 constraint notifica_utente foreign key (ID_utente) references utente(ID)
 );
